df5c8a555191d7d39fd44a246ed8a7fd76ff55c0
Arnaud LE BUNZE <arnaud.le-bunze@epitech.eu>
2025-03-05 16:45:15 +0100

    refactor de sidenav dans son propre fichier et transfere des sections vers un fichier pour les recup plus facilement

---
+++ src/app/components/Sidenav/index.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { useEffect, useState } from "react";
+import styles from "./style.module.css";
+import sections from '../../Data/sections.json';
+
+export function Sidenav() {
+
+    const [visibility, setvisibility] = useState(false)
+    useEffect(()=>{
+      setInterval(()=>{
+        if (window.scrollY >= 500) {
+          setvisibility(true);
+        } else {
+          setvisibility(false);
+        }
+      })
+    },[])
+  
+    if (visibility) {
+      return (
+        <nav className={styles.sideNav}>
+          <ul>
+            {sections.map(s => <li key={s.id}><a href={'#' + s.id}>{s.title}</a></li>)}
+          </ul>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className={styles.sideNav + ' ' + styles.invisible}>
+          <ul>
+            {sections.map(s => <li key={s.id}><a href={'#' + s.id}>{s.title}</a></li>)}
+          </ul>
+        </nav>
+      );
+    }
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,7 @@
-"use client"
 import styles from "./page.module.css";
 import { Topbar } from "./components/Topbar";
-import { useEffect, useState } from "react";
-
-const sections = ["a propos de moi", "projets", "cv", "social", "contact"];
-
-function Sidenav() {
-  const [visibility, setvisibility] = useState(false)
-  useEffect(()=>{
-    setInterval(()=>{
-      if (window.scrollY >= 500) {
-        setvisibility(true);
-      } else {
-        setvisibility(false);
-      }
-    })
-  },[])
-
-  if (visibility) {
-    return (
-      <nav className={styles.sideNav}>
-        <ul>
-          {sections.map(s => <li key={s}><a href={'#' + s}>{s}</a></li>)}
-        </ul>
-      </nav>
-    );
-  } else {
-    return (
-      <nav className={styles.sideNav + ' ' + styles.invisible}>
-        <ul>
-          {sections.map(s => <li key={s}><a href={'#' + s}>{s}</a></li>)}
-        </ul>
-      </nav>
-    );
-  }
-}
+import { Sidenav } from "./components/Sidenav";
+import sections from "./Data/sections.json";
 
 export default function Home() {
 
@@ -44,8 +11,8 @@ export default function Home() {
       <Sidenav></Sidenav>
       <main className={styles.main}>
         {sections.map((sec) => (
-          <section id={sec} key={sec}>
-            <h2>{sec}</h2>
+          <section id={sec.id} key={sec.title}>
+            <h2>{sec.title}</h2>
           </section>
         ))}
       </main>
